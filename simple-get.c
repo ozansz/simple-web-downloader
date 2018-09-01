b0af6f4396138ea99d0de80771d36813c3a0ea39
ozansz <ozan.sazak@protonmail.ch>
2018-09-01 12:34:07 +0300

    refactor

--- simple-get.c
+++ simple-get.c
@@ -1,63 +1,35 @@
+#include "sg_lib.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <curl/curl.h>
-
-#include "sg_lib.h"
 
 int main(int argc, const char **argv)
 {
-  CURL *curl_handle;
-  CURLcode res;
-
-  SimpleMemory chunk;
-
-  char user_agent[] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36"
-                      " (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"
-                      " OPR/15.0.1147.100";
-  int exit_val;
+  int res;
+  SimpleMemory mem;
 
   if (argc != 2) {
-    message_warning("Invalid number of args");
     show_usage(argv[0]);
-
-    return INVALID_ARGUMENTS;
+    return 1;
   }
 
-  chunk.memory = malloc(1);
-  chunk.size = 0;
+  __debug("Initializing variable (SimpleMemory) mem");
+  mem.memory = malloc(1);
+  mem.size = 0;
 
-  curl_global_init(CURL_GLOBAL_ALL);
+  __debug("Calling FastGet");
+  res = FastGet(argv[1], NULL, &mem);
 
-  debug("Initiating curl handler");
-  curl_handle = curl_easy_init();
-
-  debug("Setting curl opts");
-  curl_easy_setopt(curl_handle, CURLOPT_URL, argv[1]);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, user_agent);
-
-  message_info("Starting to run curl handler...");
-  res = curl_easy_perform(curl_handle);
-
-  if(res != CURLE_OK) {
-    message_error("A Curl error has occured");
-    message_error(curl_easy_strerror(res));
-    exit_val = CURL_ERROR;
+  __debug("Checking if any error has returned from FastGet");
+  if (res < 0) {
+    printf("[!!!] ECODE: %d\n", res);
   }
   else {
-    message_info("Process has done successfully");
-    printf("\n===============[BEGIN RESPONSE]===============\n\n");
-    printf("\n%s\n", chunk.memory);
-    printf("\n================[END RESPONSE]================\n\n");
+    printf("%s\n", mem.memory);
   }
 
-  curl_easy_cleanup(curl_handle);
-
-  free(chunk.memory);
-
-  curl_global_cleanup();
+  __debug("Freeing mem");
+  free(mem.memory);
 
   return 0;
 }
